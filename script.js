72e4de6ec3fc16b4ea0b87be73cf29baf1e2c768
kidde60 <kgeorgewilliam60@gmail.com>
2022-07-30 09:08:00 +0300

    Add README.md file

--- script.js
+++ script.js
@@ -1,61 +1,125 @@
-function myFunction(x) {
-    x.classList.toggle("change");
-}
-let menu = document.querySelector('.navbar');
-let navbar = document.querySelector('.navbar-list');
+function openNav() {
+  document.getElementById('myNav').style.width = '100%';
+} openNav();
 
-menu.addEventListener('click', () => {
-    if (navbar.style.display === 'block') {
-        navbar.style.display = 'none';
-    } else {
-        navbar.style.display = 'block';
-    }
-})
+/* Close */
+function closeNav() {
+  document.getElementById('myNav').style.width = '0%';
+} closeNav();
+const overlay = document.querySelector('#myNav');
+const links = document.querySelector('.menu-link');
+const links1 = document.querySelector('.menu-link1');
+const links2 = document.querySelector('.menu-link2');
+links.addEventListener('click', () => {
+  if (overlay.style.display === 'block') {
+    overlay.style.width = '100%';
+  } else {
+    overlay.style.width = '0%';
+  }
+});
+links1.addEventListener('click', () => {
+  if (overlay.style.display === 'block') {
+    overlay.style.width = '100%';
+  } else {
+    overlay.style.width = '0%';
+  }
+});
+links2.addEventListener('click', () => {
+  if (overlay.style.display === 'block') {
+    overlay.style.width = '100%';
+  } else {
+    overlay.style.width = '0%';
+  }
+});
 
-let Data = [
-    {
-        class: 'fa-solid fa-square-parking',
-        subtitle: 'Travel Plan',
-        description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna.',
-    },
-    {
-        class: 'fa-solid fa-utensils',
-        subtitle: 'Catering Service',
-        description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna.',
-    },
-    {
-        class: 'fa-solid fa-bed',
-        subtitle: 'Babysitting',
-        description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna.',
-    },
-    {
-        class: 'fa-solid fa-shirt',
-        subtitle: 'Laundry',
-        description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna.',
-    },
-    {
-        class: 'fa-solid fa-car',
-        subtitle: 'Hire Driver',
-        description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna.',
-    },
-    {
-        class: 'fa-solid fa-martini-glass',
-        subtitle: 'Bar & Drink',
-        description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna.',
-    },
-]
+// Data array
+const Data = [
+  {
+    source: 'images/car3.jpg',
+    alt: 'car-image',
+    class: 'Conducted by Travor',
+    subtitle: 'Travel Plan',
+    description: 'A travel plan is a package of actions designed by a workplace, school or other organisation.',
+  },
+  {
+    source: 'images/catering.jpg',
+    alt: 'food-image',
+    class: 'Conducted by Grace',
+    subtitle: 'Catering Service',
+    description: 'The full service means you will have kitchen staff, bartenders, servers, and the front of the.',
+  },
+  {
+    source: 'images/baby.jpg',
+    alt: 'car-image',
+    class: 'Conducted by Mariam',
+    subtitle: 'Babysitting',
+    description: 'the work of taking care of someones baby or child while that person is out, often as a paid job.',
+  },
+  {
+    source: 'images/loundary2.jpg',
+    alt: 'loundary',
+    class: 'Conducted by Joseph',
+    subtitle: 'Laundry',
+    description: 'Laundry refers to the clothes and bedding that you can wash in your washing machine.',
+  },
+  {
+    source: 'images/car1.jpg',
+    alt: 'car-image',
+    class: 'Conducted by Lawrence',
+    subtitle: 'Hire Driver',
+    description: 'Rent A Driver Uganda offers professional and experienced private drivers in Uganda.',
+  },
+  {
+    source: 'images/bar1.jpg',
+    alt: 'drinks-image',
+    class: 'Conducte by Flaviour',
+    subtitle: 'Bar & Drink',
+    description: 'Heres a list that every bartender should know to be successful at their job..',
+  },
+];
 const section = document.querySelector('.services');
 Data.forEach((item, index) => {
-    const services = document.createElement('div');
-    services.className = 'services-item'
-    section.appendChild(services);
-    let icon = document.createElement('i');
-    icon.className = Data[index].class;
-    services.appendChild(icon);
-    let heading = document.createElement('h2');
-    heading.textContent = Data[index].subtitle;
-    services.appendChild(heading);
-    let paragraph = document.createElement('p');
-    paragraph.textContent = Data[index].description;
-    services.appendChild(paragraph);
+  const container = document.createElement('div');
+  container.className = 'container';
+  const image = document.createElement('img');
+  image.src = Data[index].source;
+  image.alt = Data[index].alt;
+  container.appendChild(image);
+  section.appendChild(container);
+  const services = document.createElement('div');
+  services.className = 'services-item';
+  container.appendChild(services);
+  const heading = document.createElement('h2');
+  heading.textContent = Data[index].subtitle;
+  services.appendChild(heading);
+  const paragraph = document.createElement('p');
+  paragraph.textContent = Data[index].description;
+  services.appendChild(paragraph);
+});
+
+const service = Array.from(document.querySelectorAll('.container'));
+
+service.forEach((card, index) => {
+  if (index > 1) {
+    card.classList.add('hide');
+  }
+});
+
+const moreBtn = document.querySelector('.seeMore');
+
+const moreBtnText = (card) => {
+  if (card.classList.contains('hide')) {
+    moreBtn.innerHTML = ' See More';
+  } else {
+    moreBtn.innerHTML = 'See Less';
+  }
+};
+
+moreBtn.addEventListener('click', () => {
+  service.forEach((card, index) => {
+    if (index > 1) {
+      card.classList.toggle('hide');
+      moreBtnText(card);
+    }
+  });
 });
